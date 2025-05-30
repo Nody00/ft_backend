110e00ee95a62e208bafa645c524f653740eb3f3
nody00 <dinokrcicprof@gmail.com>
2025-05-30 10:07:02 +0200

    testing practise

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,18 +3,37 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = moduleRef.get(UsersController);
+    usersService = moduleRef.get(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return a array of users', async () => {
+      const result = ['test'];
+      jest.spyOn(usersService, 'findAll').mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve([
+            {
+              id: 1,
+              email: 'test@email.com',
+              created_at: new Date(),
+              role_id: 1,
+              deleted: false,
+            },
+          ]);
+        });
+      });
+
+      expect(await usersController.findAll()).toBe(result);
+    });
   });
 });
