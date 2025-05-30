40ba408cad6472db3c3f64c5b2733635c4d23294
nody00 <dinokrcicprof@gmail.com>
2025-05-30 10:16:36 +0200

    swagger setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,7 +15,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { PermissionsGuard } from 'src/permissions/permissions.guard';
 import { Permissions } from 'src/permissions/permissions.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @UseGuards(...[AuthGuard, PermissionsGuard])
 @Controller('users')
 export class UsersController {
